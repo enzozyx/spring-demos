bcd19153ff9d1c2fab193cb8eee3fa0765c5ac4d
enzo <enzuozyx@163.com>
2018-05-21 20:58:06 +0800

    add server-user framework!

---
+++ cloud-demos/server-user/src/main/java/com/enzo/demo/springcloud/ServerUserApplication.java
@@ -0,0 +1,14 @@
+package com.enzo.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerUserApplication.class, args);
+	}
+}
